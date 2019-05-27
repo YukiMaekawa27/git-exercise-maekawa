dab4311512358fb20abf890788838673b4607932
yuki.maekawa <yuki.maekawa@rakus.co.jp>
2019-05-27 13:48:00 +0900

    演習用プロジェクトを新規に作成

---
+++ src/main/java/com/example/GitExerciseMaekawaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitExerciseMaekawaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitExerciseMaekawaApplication.class, args);
+	}
+
+}
